a0cadfda4f49dfc57211ee66d840f3fd81adc051
Bob W. Hogg <rwhogg@linux.com>
2016-06-25 21:52:35 -0400

    Fix the script far more

--- brew-dep-tester.sh
+++ brew-dep-tester.sh
@@ -1,32 +1,81 @@
-#!/bin/ksh -x
-
-brew=`which brew`;
-prefix=`dirname $brew`;
-org="homebrew";
-tap="core";
+#!/bin/zsh -x
 
 # Hardcoded list of exceptions (libraries provided by glibc or an explicit system dependency)
-exceptions=( ld-linux-x86-64.so.2 libc.so.6 libm.so.6 ); 
+exceptions=( "ld-linux-x86-64.so.2" "libc.so.6" "libm.so.6" );
+
+function lib-available()
+{
+    formula=$1;
+    lib=$2;
+    deps=( `$brew deps $formula` );
+    for exception in $exceptions
+    do
+        if [ "x$exception" = "x$link" ]
+        then
+            return 0;
+        fi
+    done
+    for dep in $deps
+    do
+        shortdep=`basename $dep`;
+        
+        # Temporary crude test - see if the binary links to a library named "*$dep*"
+        case $lib in
+            *$shortdep*) return 0
+        esac
+    done
+    return 1;
+}
 
 function link-available()
 {
-    executable=$0;
-    formula=$1;
-    linked=( `patchelf --print-needed $executable` );
-    deps=( `$brew deps $formula` );
-    # Temporary crude test - see if the binary links to a library named "*$dep*"
-    for link in linked
+    executable=$1;
+    formula=$2;
+    linked=( `patchelf --print-needed $execdir/$executable` );
+    
+    for link in $linked
     do
-        for dep in deps
-        do
-            case $dep in
-                *$link*) return 0
-        done
+        lib-available $formula $link;
+        if [ $? -ne 0 ]
+        then
+            echo "No link available for $link in $formula!";
+            return 1;
+        fi
     done
-    return 1
-}            
+    return 0;
+}
 
-for formulafile in `dir $prefix/Library/Taps/$org/homebrew-$tap`
+function check-execs()
+{
+    formula=$1;
+    execdir=$2;
+    executables=( `dir $execdir` );
+    for executable in $executables
+    do
+        read
+        link-available $executable $formula $execdir;
+        if [ $? -ne 0 ]
+        then
+            echo "$formula is missing a dependency!";
+            read;
+            return 1;
+        fi
+    done
+}
+
+brew=`which brew`;
+prefix=`dirname $brew`/..;
+org="homebrew";
+tap="core";
+tapdir="$prefix/Library/Taps/$org/homebrew-$tap";
+
+# Check for existence of "Formula" folder
+if [ -d $tapdir/Formula ]
+then
+    tapdir="$tapdir/Formula";
+fi 
+
+for formulafile in `dir $tapdir`
 do
     formula=`basename $formulafile .rb`;
     $brew install -v $formula;
@@ -35,13 +84,6 @@ do
         echo "Installing $formula failed! Skipping."
         continue;
     fi
-    executables=`dir $prefix/opt/$formula/bin`;
-    for $executable in $executables
-    do
-        link-available $executable $formula;
-        if [ $? -ne 0 ]
-        then
-            echo "$formula is missing a dependency!";
-        fi
-    done
+    execdir="$prefix/opt/$formula/bin";
+    check-execs "$formula" "$execdir"
 done
